26e6bc51ad19781a46c2ea6e4d046305fe5c8f7c
HayyaSylvy <hannahmarieribeiro@proton.me>
2026-08-01 18:06:48 -0300

    Update

--- modules/home-manager/shell/zsh.nix
+++ modules/home-manager/shell/zsh.nix
@@ -10,7 +10,7 @@
         ZSH_DISABLE_COMPFIX = "true"; # Fixes VSCodium wrong shell warning
       };
       shellAliases = {
-	"uprotina" = "cd .nix-desktop && sudo nix flake update && git add . && git commit -m Update de rotina && git push && sudo nixos-rebuild switch --flake ~/.nix-desktop#amaterasu --upgrade";
+	"uprotina" = "cd .nix-desktop && sudo nix flake update && git add . && git commit -m Update && git push && sudo nixos-rebuild switch --flake ~/.nix-desktop#amaterasu --upgrade";
 	"rmlixo" = "sudo nix-collect-garbage -d && sudo nixos-rebuild switch --flake ~/.nix-desktop#amaterasu";
       };
       # Makes it that .zshrc and other similar files are actually on a writable folder.
