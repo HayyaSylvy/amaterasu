940d9a32774f9a778a6f9472661576fb94ff4d38
HayyaSylvy <hannahmarieribeiro@proton.me>
2026-09-02 14:18:18 -0300

    Update

--- hosts/amaterasu/configuration.nix
+++ hosts/amaterasu/configuration.nix
@@ -223,27 +223,27 @@ in
 	(final: _prev: {
       	pnpm_10_29_2 = final.pnpm_10;
     	})
-	(final: prev: {
-        vesktop = prev.vesktop.overrideAttrs (old: {
-          preBuild = ''
-            cp -r ${prev.electron.dist} electron-dist
-            chmod -R u+w electron-dist
-          '';
-          buildPhase = ''
-            runHook preBuild
-
-            pnpm build
-            pnpm exec electron-builder \
-              --dir \
-              -c.asarUnpack="**/*.node" \
-              -c.electronDist="electron-dist" \
-              -c.electronVersion=${prev.electron.version}
-
-            runHook postBuild
-          '';
-        });
-        })
-    	(_: prev: {
+	#(final: prev: {
+        #vesktop = prev.vesktop.overrideAttrs (old: {
+        #  preBuild = ''
+        #    cp -r ${prev.electron.dist} electron-dist
+        #    chmod -R u+w electron-dist
+        #  '';
+        #  buildPhase = ''
+        #    runHook preBuild
+#
+ #           pnpm build
+ #           pnpm exec electron-builder \
+  #            --dir \
+   #           -c.asarUnpack="**/*.node" \
+    #          -c.electronDist="electron-dist" \
+     #         -c.electronVersion=${prev.electron.version}
+#
+ #           runHook postBuild
+  #        '';
+   #     });
+    #    })
+    	( final: prev: {
       		openldap = prev.openldap.overrideAttrs {
         	doCheck = !prev.stdenv.hostPlatform.isi686;
       	};
